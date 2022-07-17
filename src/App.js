99b9ee9928f0e9b83cfe03ddf52ffd389d70f111
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-17 23:19:09 +0200

    Displaying the data.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import List from "./List";
+import data from "./data";
+import { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className="container">
-        <h3>0 Birthdays today</h3>
-        <List />
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
         <button onClick={() => console.log("clicked me")}>Clear all</button>
       </section>
     </main>
